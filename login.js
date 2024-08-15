25f43fe4bb5c41186fa3d8aaaa7443ddd0e01d13
Jenish Limbu <jlimbu539@gmail.com>
2024-08-15 22:27:19 +0545

    replaced home with index

--- login.js
+++ login.js
@@ -20,7 +20,7 @@ function login_submit() {
     sessionStorage.setItem("loggedIn", "true");
     event.preventDefault();
     sessionStorage.setItem("newRegister", "false");
-    window.location.href = "./userpanel/home.html";
+    window.location.href = "./userpanel/index.html";
   } else {
     document.getElementById("loginErr").style.display = "block";
   }
